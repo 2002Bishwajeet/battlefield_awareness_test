7513fb94af75bbd107cfccae99562130b42c4082
Bishwajeet Parhi <bishwajeet.techmaster@gmail.com>
2023-11-22 14:08:13 +0530

    add drawer

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,7 @@ import 'package:flutter_blue_plus/flutter_blue_plus.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:latlong2/latlong.dart';
+import 'package:snapping_sheet/snapping_sheet.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -28,6 +29,10 @@ final bluetoothDevicesProvider = StreamProvider<List<BluetoothDevice>>((ref) {
   return FlutterBluePlus.scanResults.map((event) => event.map((e) => e.device).toList());
 });
 
+final currentBluetoothProvider = StateProvider<BluetoothDevice?>((ref) {
+  return;
+});
+
 class MainApp extends ConsumerWidget {
   const MainApp({super.key});
 
@@ -48,7 +53,7 @@ class HomeApp extends ConsumerStatefulWidget {
   ConsumerState<ConsumerStatefulWidget> createState() => _HomeAppState();
 }
 
-class _HomeAppState extends ConsumerState<HomeApp> {
+class _HomeAppState extends ConsumerState<HomeApp> with TickerProviderStateMixin {
   /// Wether the bluetooth device is on or off
   ValueNotifier<bool> bluetoothOn = ValueNotifier(false);
 
@@ -60,8 +65,11 @@ class _HomeAppState extends ConsumerState<HomeApp> {
   /// Would be programatically updated by the bluetooth device connected
   final ValueNotifier<List<LatLng>> markers = ValueNotifier([]);
 
+  late final ScrollController scrollController;
+
   @override
   void initState() {
+    scrollController = ScrollController();
     mapController = MapController();
     super.initState();
   }
@@ -76,6 +84,7 @@ class _HomeAppState extends ConsumerState<HomeApp> {
 
   @override
   void dispose() {
+    scrollController.dispose();
     mapController.dispose();
     bluetoothOn.dispose();
     markers.dispose();
@@ -85,117 +94,229 @@ class _HomeAppState extends ConsumerState<HomeApp> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('BattleField Test'),
-        actions: [
-          ValueListenableBuilder(
-              valueListenable: bluetoothOn,
-              builder: (context, value, _) {
-                return IconButton(
-                    icon: Icon(value ? Icons.bluetooth : Icons.bluetooth_disabled),
-                    onPressed: () async {
-                      if (Platform.isAndroid) {
-                        if (!value) FlutterBluePlus.turnOn();
-                      }
-                    });
-              })
-        ],
-      ),
-      drawer: Drawer(
-        semanticLabel: 'Bluetooth Device Scanner',
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            StreamBuilder<bool>(
-                stream: FlutterBluePlus.isScanning,
-                builder: (context, snapshot) {
-                  return DrawerHeader(
-                    child: Row(
-                      children: [
-                        Flexible(
-                            child: Text(snapshot.hasData && snapshot.data!
-                                ? 'Scanning for Bluetooth Devices...'
-                                : 'Press Button to start scanning')),
-                        IconButton.filledTonal(
-                            onPressed: () {
-                              if (!bluetoothOn.value) {
-                                ScaffoldMessenger.of(context).showSnackBar(
-                                  const SnackBar(
-                                    content: Text('Please Turn on Bluetooth'),
-                                    showCloseIcon: true,
-                                    behavior: SnackBarBehavior.floating,
-                                  ),
-                                );
-                                return;
-                              }
-                              if (!FlutterBluePlus.isScanningNow) {
-                                FlutterBluePlus.startScan(continuousUpdates: true);
-                              }
-                            },
-                            icon: Icon(snapshot.hasData && snapshot.data! ? Icons.refresh : Icons.add))
-                      ],
-                    ),
-                  );
-                }),
-            ref.watch(bluetoothDevicesProvider).when(
-                data: (data) {
-                  return Expanded(
-                    child: ListView(
-                      children: data
-                          .map((e) => ListTile(
-                                title: Text(e.platformName),
-                                subtitle: Text(e.remoteId.str),
-                                onTap: () {
-                                  e.connect(autoConnect: true).onError((error, stackTrace) {
-                                    ScaffoldMessenger.of(context).showSnackBar(
-                                      SnackBar(
-                                        content: Text(error.toString()),
-                                        showCloseIcon: true,
-                                        behavior: SnackBarBehavior.floating,
-                                      ),
-                                    );
-                                  }).then((value) => ScaffoldMessenger.of(context).showSnackBar(
-                                        SnackBar(
-                                          content: Text('Connected to ${e.platformName}'),
-                                          showCloseIcon: true,
-                                          behavior: SnackBarBehavior.floating,
-                                        ),
-                                      ));
-
-                                  Navigator.of(context).pop();
-                                },
-                              ))
-                          .toList(),
-                    ),
-                  );
-                },
-                error: (error, stackTrace) {
-                  return ErrorWidget(error);
-                },
-                loading: () => const Center(
-                      child: CircularProgressIndicator.adaptive(),
-                    )),
+        appBar: AppBar(
+          title: const Text('BattleField Test'),
+          actions: [
+            ValueListenableBuilder(
+                valueListenable: bluetoothOn,
+                builder: (context, value, _) {
+                  return IconButton(
+                      tooltip: 'Bluetooth Toggle',
+                      icon: Icon(value ? Icons.bluetooth : Icons.bluetooth_disabled),
+                      onPressed: () async {
+                        if (Platform.isAndroid) {
+                          if (!value) FlutterBluePlus.turnOn();
+                        }
+                      });
+                })
           ],
         ),
-      ),
-      body: FlutterMap(
-        mapController: mapController,
-        options: const MapOptions(
-          initialCenter: LatLng(28.631131939823522, 77.09205778819896),
-          initialZoom: 9.2,
-        ),
-        children: [
-          TileLayer(
-            urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
-            userAgentPackageName: 'com.biswa.battlefield_awareness_test',
+        drawer: Drawer(
+          semanticLabel: 'Bluetooth Device Scanner',
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              StreamBuilder<bool>(
+                  stream: FlutterBluePlus.isScanning,
+                  builder: (context, snapshot) {
+                    return DrawerHeader(
+                      child: Row(
+                        children: [
+                          Flexible(
+                              child: Text(snapshot.hasData && snapshot.data!
+                                  ? 'Scanning for Bluetooth Devices...'
+                                  : 'Press Button to start scanning')),
+                          IconButton.filledTonal(
+                              onPressed: () {
+                                if (!bluetoothOn.value) {
+                                  ScaffoldMessenger.of(context).showSnackBar(
+                                    const SnackBar(
+                                      content: Text('Please Turn on Bluetooth'),
+                                      showCloseIcon: true,
+                                      behavior: SnackBarBehavior.floating,
+                                    ),
+                                  );
+                                  return;
+                                }
+                                if (!FlutterBluePlus.isScanningNow) {
+                                  FlutterBluePlus.startScan(continuousUpdates: true);
+                                }
+                              },
+                              icon: Icon(snapshot.hasData && snapshot.data! ? Icons.refresh : Icons.add))
+                        ],
+                      ),
+                    );
+                  }),
+              ref.watch(bluetoothDevicesProvider).when(
+                  data: (data) {
+                    return Expanded(
+                      child: ListView(
+                        children: data.map((e) => TileWidget(device: e)).toList(),
+                      ),
+                    );
+                  },
+                  error: (error, stackTrace) {
+                    return ErrorWidget(error);
+                  },
+                  loading: () => const Center(
+                        child: CircularProgressIndicator.adaptive(),
+                      )),
+            ],
           ),
-          ValueListenableBuilder(
-              valueListenable: markers,
+        ),
+        body: SnappingSheet(
+          /// Show Sheet only when bluetooth device is connected
+          sheetBelow:
+              // ref.watch(currentBluetoothProvider) != null ?
+              SnappingSheetContent(
+                  childScrollController: scrollController,
+                  draggable: true,
+                  child: ListView(
+                    controller: scrollController,
+                    reverse: true,
+                    children: const [],
+                  ))
+          // : null
+          ,
+          // grabbing: ref.watch(currentBluetoothProvider) != null ? const GrabbingWidget() : const SizedBox.shrink(),
+          grabbing: const GrabbingWidget(),
+          grabbingHeight: 75,
+          snappingPositions: const [
+            SnappingPosition.factor(
+              positionFactor: 0.0,
+              snappingCurve: Curves.easeOutExpo,
+              snappingDuration: Duration(seconds: 1),
+              grabbingContentOffset: GrabbingContentOffset.top,
+            ),
+            SnappingPosition.factor(
+              snappingCurve: Curves.elasticOut,
+              snappingDuration: Duration(milliseconds: 1750),
+              positionFactor: 0.5,
+            ),
+            SnappingPosition.factor(
+              grabbingContentOffset: GrabbingContentOffset.bottom,
+              snappingCurve: Curves.easeInExpo,
+              snappingDuration: Duration(seconds: 1),
+              positionFactor: 0.9,
+            ),
+          ],
+          child: FlutterMap(
+            mapController: mapController,
+            options: const MapOptions(
+              initialCenter: LatLng(28.631131939823522, 77.09205778819896),
+              initialZoom: 9.2,
+            ),
+            children: [
+              TileLayer(
+                urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+                userAgentPackageName: 'com.biswa.battlefield_awareness_test',
+              ),
+              ValueListenableBuilder(
+                  valueListenable: markers,
+                  builder: (context, value, _) {
+                    return MarkerLayer(
+                        markers: value.map((e) => Marker(point: e, child: const Icon(Icons.pin_drop))).toList());
+                  }),
+            ],
+          ),
+        ));
+  }
+}
+
+class TileWidget extends ConsumerStatefulWidget {
+  final BluetoothDevice device;
+  const TileWidget({super.key, required this.device});
+
+  @override
+  ConsumerState<ConsumerStatefulWidget> createState() => _TileWidgetState();
+}
+
+class _TileWidgetState extends ConsumerState<TileWidget> {
+  ValueNotifier<bool> loading = ValueNotifier(false);
+
+  bool connecting = false;
+
+  @override
+  void dispose() {
+    loading.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final currentSelectedDevice = ref.watch(currentBluetoothProvider);
+    return ListTile(
+      title: Text(widget.device.platformName),
+      subtitle: Text(widget.device.remoteId.str),
+      trailing: currentSelectedDevice != null && currentSelectedDevice == widget.device
+          ? const Text('Connected')
+          : ValueListenableBuilder(
+              valueListenable: loading,
               builder: (context, value, _) {
-                return MarkerLayer(
-                    markers: value.map((e) => Marker(point: e, child: const Icon(Icons.pin_drop))).toList());
+                return value ? const CircularProgressIndicator.adaptive() : const SizedBox.shrink();
               }),
+      onTap: () {
+        if (connecting) return;
+        loading.value = true;
+        connecting = true;
+        widget.device.connect(autoConnect: true).onError((error, stackTrace) {
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(
+              content: Text(error.toString()),
+              showCloseIcon: true,
+              behavior: SnackBarBehavior.floating,
+            ),
+          );
+        }).then((_) {
+          ref.read(currentBluetoothProvider.notifier).update((_) => widget.device);
+          return ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(
+              content: Text('Connected to ${widget.device.platformName}'),
+              showCloseIcon: true,
+              behavior: SnackBarBehavior.floating,
+            ),
+          );
+        });
+        connecting = false;
+        loading.value = false;
+
+        Navigator.of(context).pop();
+      },
+    );
+  }
+}
+
+class GrabbingWidget extends StatelessWidget {
+  const GrabbingWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: const BorderRadius.vertical(top: Radius.circular(20)),
+        boxShadow: [
+          BoxShadow(blurRadius: 25, color: Colors.black.withOpacity(0.2)),
+        ],
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Container(
+            margin: const EdgeInsets.only(top: 20),
+            width: 100,
+            height: 7,
+            decoration: BoxDecoration(
+              color: Colors.grey,
+              borderRadius: BorderRadius.circular(5),
+            ),
+          ),
+          Container(
+            color: Colors.grey[200],
+            height: 2,
+            margin: const EdgeInsets.all(15).copyWith(top: 0, bottom: 0),
+          )
         ],
       ),
     );
